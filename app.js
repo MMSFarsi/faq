30b99c648edbaaa79a2b27b69628829e6ece9356
mmsfarsi <mirmdsalmanfarsi.1@gmail.com>
2024-09-03 08:22:19 -0700

    d

---
+++ app.js
@@ -0,0 +1,14 @@
+const faq = document.querySelector(".faq");
+const reverse = document.querySelector(".reverse-icon");
+const faqTitle = document.querySelectorAll(".faq-title");
+
+const rev = () => faq.classList.toggle("active");
+reverse.addEventListener("click", rev);
+
+
+let darkbtn = document.querySelector(".light");
+let body = document.querySelector("body");
+let darkmode=()=>{
+    body.classList.toggle("darkmode");
+}
+darkbtn.addEventListener("click",darkmode )
